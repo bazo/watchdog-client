078a04ca30c80aea44655226c92028147a7938be
bazo <martin@bazo.sk>
2012-04-30 17:01:02 +0200

    should send request

--- Bazo/Watchdog/Client.php
+++ Bazo/Watchdog/Client.php
@@ -9,12 +9,33 @@ class Client
 {
 	private 
 		$appId,
-		$appKey
+		$appKey,
+		
+		$endPoint = 'http://watchdog.local/api/log'	
 	;
 	
 	public function __construct($appId, $appKey)
 	{
 		$this->appId = $appId;
 		$this->appKey = $appKey;
+	}
+	
+	public function log($message, $level = 'error')
+	{
+		$data = array(
+			'appId' => $this->appId,
+			'appKey' => $this->appKey,
+			'message' => $message,
+			'level' => $level
+		);
+		
+		$ch = curl_init($this->endPoint);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+		$response = curl_exec($ch);
+		curl_close($ch);
+		return json_decode($response);
 	}
 }
