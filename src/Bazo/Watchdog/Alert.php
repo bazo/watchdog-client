0cd54b5fe50bc6e0a6148892cd5bc9bd4640ad4b
Martin Bazik <martin@bazo.sk>
2013-10-29 13:36:33 +0100

    Pure watchdog client

---
+++ src/Bazo/Watchdog/Alert.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Bazo\Watchdog;
+
+/**
+ * Alert
+ *
+ * @author Martin
+ */
+abstract class Alert
+{
+
+	const ERROR = 'error';
+	const INFO = 'info';
+	const SUCCESS = 'success';
+	const NOTICE = 'notice';
+
+
+}
+
--- src/Bazo/Watchdog/WatchdogClient.php
+++ src/Bazo/Watchdog/WatchdogClient.php
@@ -1,67 +1,74 @@
 <?php
+
 namespace Bazo\Watchdog;
+
 /**
  * Description of WatchdogClient
  *
  * @author Martin
  */
-class Client
+class WatchdogClient
 {
-	private 
-		$appId,
-		$appKey,
-		
-		$endPoint = '/api/log'	
-	;
-	
-	public function __construct($appId, $appKey, $server = 'http://watchdog.pagodabox.com')
+
+	private $appId;
+	private $appKey;
+	private $endPoint = '/api/log';
+
+
+	public function __construct($appId, $appKey, $server)
 	{
 		$this->appId = $appId;
 		$this->appKey = $appKey;
-		$this->endPoint = $server.$this->endPoint;
+		$this->endPoint = $server . $this->endPoint;
 	}
-	
+
+
 	public function log($message, $level = Alert::ERROR)
 	{
-		$data = array(
+		$data = [
 			'appId' => $this->appId,
 			'appKey' => $this->appKey,
 			'message' => $message,
 			'level' => $level
-		);
-		
+		];
+
 		return $this->sendData($data);
 	}
-	
+
+
 	public function logNette($netteMessage, $level = Alert::ERROR)
 	{
-		
+
 		if (is_array($netteMessage)) {
 			$message = implode(' ', $netteMessage);
 		}
-		
-		$data = array(
+
+		$data = [
 			'appId' => $this->appId,
 			'appKey' => $this->appKey,
 			'message' => $message,
 			'level' => $level,
-			'nette' => true,
+			'nette' => TRUE,
 			'netteMessage' => $netteMessage
-		);
-		
+		];
+
 		return $this->sendData($data);
 	}
-	
+
+
 	private function sendData($data)
 	{
-		$data = array('data' => json_encode($data));
+		$data = ['data' => json_encode($data)];
 		$ch = curl_init($this->endPoint);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-		curl_setopt($ch, CURLOPT_POST, true);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+		curl_setopt($ch, CURLOPT_POST, TRUE);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 		$response = curl_exec($ch);
 		curl_close($ch);
 		return json_decode($response);
 	}
+
+
 }
+
